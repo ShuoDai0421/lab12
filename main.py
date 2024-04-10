5c735d4bd822d397adb30d968a4c36f6a90c564a
ShuoDai0421 <166658131+ShuoDai0421@users.noreply.github.com>
2024-04-10 14:34:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 def load_data():
-    data = pd.read_csv(r"C:\Users\shuod\Downloads\Lab12\Lab12\car_data.csv") 
+    data = pd.read_csv('car_data.csv') 
     return data
 
 data = load_data()
